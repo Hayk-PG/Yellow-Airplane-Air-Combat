54f9b7315ca3f32fb17c0e3d47d1db8deb207507
Hayk-PG <pautikgames@gmail.com>
2023-06-04 15:11:59 -0400

    Imported new asset into the project

---
+++ Assets/MobilePostProcess/Example/Movement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 movementVector;
+    void Update()
+    {
+        transform.position += movementVector * Time.deltaTime; 
+    }
+}
