37161ce7355239fc2d30dcb8d3b0fffbfb961df8
Hayk-PG <pautikgames@gmail.com>
2023-11-26 23:19:08 -0500

    Improved Airplane Camera Tracking for Smooth Movement

--- Assets/Scripts/CameraScripts/AirplaneCameraController.cs
+++ Assets/Scripts/CameraScripts/AirplaneCameraController.cs
@@ -8,11 +8,6 @@ public class AirplaneCameraController : MonoBehaviour
 
     private Rigidbody2D _airplaneRigidbody;
 
-    private float _desiredY;
-    private float _currentY;
-    private float _desiredX;
-    private float _currentX;
-
 
 
 
@@ -28,66 +23,10 @@ public class AirplaneCameraController : MonoBehaviour
 
     private void TrackPlayerAirplane()
     {
-        _currentY = transform.position.y;
-        _currentX = transform.position.x;
-
-        ClampVerticalPosition();
-        ClampHorizontalPosition();
-        UpdateDesiredVerticalPosition(_airplaneRigidbody.position);
-        UpdateDesiredHorizontalPosition(_airplaneRigidbody.position);
-        UpdateCameraPosition();
-    }
-
-    private void ClampVerticalPosition()
-    {
-        if (_currentY >= Reference.Manager.MapBounds.Max.y)
-        {
-            _currentY = Reference.Manager.MapBounds.Max.y;
-        }
-
-        else if (_currentY <= Reference.Manager.MapBounds.Min.y)
-        {
-            _currentY = Reference.Manager.MapBounds.Min.y;
-        }
-    }
-
-    private void ClampHorizontalPosition()
-    {
-        if (_currentX >= Reference.Manager.MapBounds.Max.x)
-        {
-            _currentX = Reference.Manager.MapBounds.Max.x;
-        }
-
-        else if (_currentX <= Reference.Manager.MapBounds.Min.x)
-        {
-            _currentX = Reference.Manager.MapBounds.Min.x;
-        }
-    }
-
-    private void UpdateDesiredVerticalPosition(Vector2 playerRigidbodyPosition)
-    {
-        bool isPlayerAboveBoxCenterAndWithinVerticalBounds = playerRigidbodyPosition.y > _boxCollider.bounds.center.y && _currentY <= Reference.Manager.MapBounds.Max.y;
-        bool isPlayerBelowBoxCenterAndWithinVerticalBounds = playerRigidbodyPosition.y < _boxCollider.bounds.center.y && _currentY >= Reference.Manager.MapBounds.Min.y;
-
-        if (isPlayerAboveBoxCenterAndWithinVerticalBounds || isPlayerBelowBoxCenterAndWithinVerticalBounds)
-        {
-            _desiredY = Mathf.Lerp(_currentY, playerRigidbodyPosition.y, 5 * Time.deltaTime);
-        }
-    }
-
-    private void UpdateDesiredHorizontalPosition(Vector2 playerRigidbodyPosition)
-    {
-        bool isPlayerRightOfBoxCenterAndWithinHorizontalBounds = playerRigidbodyPosition.x > _boxCollider.bounds.center.x && _currentX <= Reference.Manager.MapBounds.Max.x;
-        bool isPlayerLeftOfBoxCenterAndWithinHorizontalBounds = playerRigidbodyPosition.x < _boxCollider.bounds.center.x && _currentX >= Reference.Manager.MapBounds.Min.x;
-
-        if (isPlayerRightOfBoxCenterAndWithinHorizontalBounds || isPlayerLeftOfBoxCenterAndWithinHorizontalBounds)
-        {
-            _desiredX = Mathf.Lerp(_currentX, playerRigidbodyPosition.x, 5 * Time.deltaTime);
-        }
-    }
-
-    private void UpdateCameraPosition()
-    {
-        transform.position = new Vector3(_desiredX, _desiredY, -10);
+        Vector3 targetPosition = Vector2.Lerp(transform.position, _airplaneRigidbody.position, 5 * Time.deltaTime);
+        targetPosition.x = Mathf.Clamp(targetPosition.x, Reference.Manager.MapBounds.Min.x, Reference.Manager.MapBounds.Max.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, Reference.Manager.MapBounds.Min.y, Reference.Manager.MapBounds.Max.y);
+        targetPosition.z = -10;
+        transform.position = targetPosition;
     }
 }
